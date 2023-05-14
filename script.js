342c0fe431bca7e0775ffeceae3e0bc598b60510
Lavina <129137303+WHT-RBT@users.noreply.github.com>
2023-05-14 14:38:38 -0700

    updated time block coloring and save data to local storage coding in JavaScript. Corrected extra div code for hour class in HTML file

--- script.js
+++ script.js
@@ -2,49 +2,45 @@ $(document).ready(function() {
 
 // Get the current date and time using Day.js
 var now = dayjs();
+var currentHour = dayjs().format('H');
 
 // Display the date on the page header
 $("#currentDay").text(now.format("dddd, MMMM D, YYYY"));
 
 //time block coloring
-colorTimeBlocks();
-setInterval(colorTimeBlocks, 60000);
+$(".time-block").each(function() {
+var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-function colorTimeBlocks() {
-  $(".time-block").each(function() {
-
-var blockHour = parseInt($(this).attr("data-hour"));
-var currentHour = parseInt(now.format("H"));
-
-// Remove any previous class
+//this removeds any existing classes
 $(this).removeClass("past present future");
 
-// Add the appropriate class based on past, present, future
+//adding appropriate class to time block
 if (blockHour < currentHour) {
   $(this).addClass("past");
-} else if (blockHour === currentHour) {
+} else if (blockHour == currentHour) {
   $(this).addClass("present");
 } else {
   $(this).addClass("future");
 }
 });
-}
-
-//Load any saved data from local storage
-$(".description").each(function() {
-  var id = $(this).attr("id");
-  var data = localStorage.getItem(id);
-
-  if (data !== null) {
-    $(this).val(data);
-  }
-});
 
 //Save data to local storage when save button is clicked
 $(".saveBtn").on("click", function() {
-  var id = $(this).siblings(".description").attr("id");
-  var data = $(this).siblings(".description").val();
+var id = $(this).siblings(".description").attr("id");
+var data = $(this).siblings(".description").val();
+var savedData = { id: id, data: data };
+localStorage.setItem(id, JSON.stringify(savedData));
+});
 
-  localStorage.setItem(id, data);
+//Load saved data from local storage
+$(".description").each(function() {
+var id = $(this).attr("id");
+var savedData = localStorage.getItem(id);
+if (savedData !== null) {
+savedData = JSON.parse(savedData);
+if (id === savedData.id) {
+$(this).val(savedData.data);
+}
+}
 });
 });
